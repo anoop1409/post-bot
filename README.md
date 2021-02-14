02ab68074d6dda6462c982a6233ff830e8cf533a
Kush Raghani <kush.raghani@gmail.com>
2021-02-14 15:17:30 +0530

    removing chrome driver path, addressing review comments

--- README.md
+++ README.md
@@ -13,14 +13,15 @@ A LinkedIn post scraping bot
 
 To run the python script for web scraping, navigate to the web-scraper folder and follow the below given steps:
 
+- Make sure chrome driver path is added to PATH environment variable
 - Rename sample-settings.py file to settings.py
 - Within settings.py, enter credentials for the LinkedIn account which will to be used for scraping. Also enter the user who's posts have to be scraped and the search terms. Sample values:
 
-`LINKEDIN_USERNAME = 'test-email@testtest.com'`
-`LINKEDIN_PASSWORD = 'testpassword'`
-`LINKEDIN_POSTS_URL = 'https://www.linkedin.com/in/dummy-user/detail/recent-activity/shares/'`
+`LINKEDIN_USERNAME = 'test-email@testtest.com'`  
+`LINKEDIN_PASSWORD = 'testpassword'`  
+`LINKEDIN_POSTS_URL = 'https://www.linkedin.com/in/dummy-user/detail/recent-activity/shares/'`  
 `POSTS_SEARCH_TERMS = 'Python,Django'`
 
 - Once the above changes are done, run python3 index.py from inside the web-scraper folder path. The posts which satisfy the given criteria of search terms for the given user, will be printed in the terminal.
 
-- To run selenium in headless mode, make the following changes in settings.py file `TEST_MODE_HEADLESS = 'true'` otherwise make this as `false`
+- To run selenium in headless mode, make the following changes in settings.py file `SCRAPE_MODE_HEADLESS = 'true'` otherwise make this as `false`
--- web-scraper/index.py
+++ web-scraper/index.py
@@ -5,62 +5,65 @@ from bs4 import BeautifulSoup
 import re
 
 #Import configuration data
-from settings import LINKEDIN_USERNAME,LINKEDIN_PASSWORD,LINKEDIN_LOGIN_URL,LINKEDIN_POSTS_URL,POSTS_SEARCH_TERMS,CHROME_DRIVER_PATH,TEST_MODE_HEADLESS
+import settings
 
 #Initialize Selenium web driver for Chrome
 options = Options()
 
-# Running selenium in headless mode if TEST_MODE_HEADLESS is true
-if(TEST_MODE_HEADLESS == 'true'):
+# Running selenium in headless mode if SCRAPE_MODE_HEADLESS is true
+if(settings.SCRAPE_MODE_HEADLESS == 'true'):
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-browser = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
+
+# Set chromedriver location in PATH
+browser = webdriver.Chrome(options=options)
 
 #Open login page
-browser.get(LINKEDIN_LOGIN_URL)
+browser.get(settings.LINKEDIN_LOGIN_URL)
 
 #Enter login info:
 elementID = browser.find_element_by_id('username')
-elementID.send_keys(LINKEDIN_USERNAME)
+elementID.send_keys(settings.LINKEDIN_USERNAME)
 
 elementID = browser.find_element_by_id('password')
-elementID.send_keys(LINKEDIN_PASSWORD)
+elementID.send_keys(settings.LINKEDIN_PASSWORD)
 
 elementID.submit()
 
 #Open posts page for a given user
-browser.get(LINKEDIN_POSTS_URL)
+browser.get(settings.LINKEDIN_POSTS_URL)
 
 #BS4 setup for posts page
 src = browser.page_source
 soup = BeautifulSoup(src, 'lxml')
 
-def getAllPosts():
+units_of_time = ["second","minute","hour"]
+def get_all_posts():
     posts = soup.find_all('div',class_="occludable-update")
-    filteredPosts = []
+    filtered_posts = []
     for post in posts:
         feed = post.find('span',class_="feed-shared-actor__sub-description")
        
         if (feed != None):
-            relativePublishTimeText = feed.find('span',class_="visually-hidden")
+            relative_publish_time_text = feed.find('span',class_="visually-hidden")
             
             # Posts do not have a timestamp section; It contain text stating when a post was published
             # Text is relative to current time. Examples include 1 hour ago, 20 hours ago, 2 days ago etc.
             # Filtering posts which have 'hour' in text, to get only new posts
-            if("hour" in relativePublishTimeText.text):
-                filteredPosts.append(post.find('span',class_="break-words"))
-    return filteredPosts
+            if any(unit in relative_publish_time_text.text for unit in units_of_time):
+                filtered_posts.append(post.find('span',class_="break-words"))
+    return filtered_posts
 
-def filterPosts(posts,searchTerm):
-    return posts.find(text=re.compile(searchTerm, re.IGNORECASE))
+def filter_posts(posts,search_term):
+    return posts.find(text=re.compile(search_term, re.IGNORECASE))
 
 #Search terms list
-searchTermArray = POSTS_SEARCH_TERMS.split(',')
+search_term_array = settings.POSTS_SEARCH_TERMS.split(',')
 
-allUserPosts = getAllPosts()
+all_user_posts = get_all_posts()
 
 #Looping over posts
-for posts in allUserPosts:
-    for searchTerm in searchTermArray:
-        if filterPosts(posts,searchTerm):
-            print(searchTerm, posts.get_text())
+for posts in all_user_posts:
+    for search_term in search_term_array:
+        if filter_posts(posts,search_term):
+            print(search_term, posts.get_text())
